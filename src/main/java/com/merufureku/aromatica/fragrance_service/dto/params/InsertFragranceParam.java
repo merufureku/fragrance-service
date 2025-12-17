9046964ee138543ab27c6d600a6ddb6cc3cd2ea6
Mark Christian Mercado <markchristian113@gmail.com>
2025-12-17 12:08:25 +0800

    added minor validaitons

--- src/main/java/com/merufureku/aromatica/fragrance_service/dto/params/InsertFragranceParam.java
+++ src/main/java/com/merufureku/aromatica/fragrance_service/dto/params/InsertFragranceParam.java
@@ -1,5 +1,14 @@
 package com.merufureku.aromatica.fragrance_service.dto.params;
 
-public record InsertFragranceParam(String name, String brand, String description,
-                                   String type, String countryOfOrigin, String gender,
-                                   Integer releaseYear, String text) {}
+import jakarta.validation.constraints.NotBlank;
+
+public record InsertFragranceParam(
+        @NotBlank(message = "Fragrance name cannot be null") String name,
+        @NotBlank(message = "Fragrance brand cannot be null") String brand,
+        @NotBlank(message = "Fragrance description cannot be null") String description,
+        @NotBlank(message = "Fragrance type cannot be null") String type,
+        @NotBlank(message = "Fragrance origin cannot be null") String countryOfOrigin,
+        @NotBlank(message = "Fragrance gender cannot be null") String gender,
+        @NotBlank(message = "Fragrance release year cannot be null") Integer releaseYear,
+        String text
+) {}
